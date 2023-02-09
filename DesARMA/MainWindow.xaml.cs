b0b4f54ad18c45bf3920e00654c9ca54875ca6fd
MaxSho <max_max_max_max@ukr.net>
2023-02-09 11:14:12 +0200

    commit new method

--- DesARMA/MainWindow.xaml.cs
+++ DesARMA/MainWindow.xaml.cs
@@ -65,17 +65,7 @@ namespace DesARMA
         ModelContext modelContext = new ModelContext();
         public User CurrentUser { get; set; } = null!;
 
-        public static string CreateMD5(string input)
-        {
-            // Use input string to calculate MD5 hash
-            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
-            {
-                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
-                byte[] hashBytes = md5.ComputeHash(inputBytes);
 
-                return Convert.ToHexString(hashBytes); // .NET 5 +
-            }
-        }
         public MainWindow()
         {
             try
@@ -99,6 +89,17 @@ namespace DesARMA
             {
                 System.Windows.MessageBox.Show(e.Message);
                 Environment.Exit(0);
+            }
+        }
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                return Convert.ToHexString(hashBytes); // .NET 5 +
             }
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
